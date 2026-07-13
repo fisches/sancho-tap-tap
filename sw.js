720e3ab85083bd403cc84831dc8696e0295dd73a
Sylvain Fischer <sylvain1991@gmail.com>
2026-07-13 20:16:33 +0200

    Compact overlays for short viewports

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sancho-tap-tap-shell-v78";
-const RUNTIME_CACHE = "sancho-tap-tap-runtime-v78";
+const SHELL_CACHE = "sancho-tap-tap-shell-v79";
+const RUNTIME_CACHE = "sancho-tap-tap-runtime-v79";
 const APP_SHELL = [
   "./",
   "./index.html",
