bb2bdd4b2082e970a6cf8b4912eb944326270a07
Sylvain Fischer <sylvain1991@gmail.com>
2026-07-13 12:49:39 +0200

    Expose parent access when locked

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sancho-tap-tap-shell-v23";
-const RUNTIME_CACHE = "sancho-tap-tap-runtime-v23";
+const SHELL_CACHE = "sancho-tap-tap-shell-v24";
+const RUNTIME_CACHE = "sancho-tap-tap-runtime-v24";
 const APP_SHELL = [
   "./",
   "./index.html",
