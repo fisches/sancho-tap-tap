f4dfd30a9e033beb5a77bc19a6e90bca9a807044
Sylvain Fischer <sylvain1991@gmail.com>
2026-07-13 20:13:05 +0200

    Compact menu for short viewports

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sancho-tap-tap-shell-v77";
-const RUNTIME_CACHE = "sancho-tap-tap-runtime-v77";
+const SHELL_CACHE = "sancho-tap-tap-shell-v78";
+const RUNTIME_CACHE = "sancho-tap-tap-runtime-v78";
 const APP_SHELL = [
   "./",
   "./index.html",
