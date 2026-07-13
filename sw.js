9de2dc13be5a22469fc3f6de6a9e38df53294847
Sylvain Fischer <sylvain1991@gmail.com>
2026-07-13 19:23:21 +0200

    Align resume menu label

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const SHELL_CACHE = "sancho-tap-tap-shell-v63";
-const RUNTIME_CACHE = "sancho-tap-tap-runtime-v63";
+const SHELL_CACHE = "sancho-tap-tap-shell-v64";
+const RUNTIME_CACHE = "sancho-tap-tap-runtime-v64";
 const APP_SHELL = [
   "./",
   "./index.html",
